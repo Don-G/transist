506b0fc347439ca0959094416b2fb7041e70a543
Osaru Agbonaye <agbonayeosaru@gmail.com>
2016-11-19 08:31:46 +0100

    Add files via upload

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+/*Kartero App Configuration*/
+var krms_driver_config ={		
+	'ApiUrl':"http://transistgroup.com/api",					
+	'DialogDefaultTitle':"Transist",	
+	'PushProjectID':"332724592782",	
+	'APIHasKey':""
+};
